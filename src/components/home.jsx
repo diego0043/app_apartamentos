ea5edb7b9d78b06f30807e206afbc7ca04399f17
Diego Valencia <ve18007@ues.edu.sv>
2022-12-07 19:49:16 -0600

    Adding tailwindcss to the project

---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import styles from '../styles.css'
+
+export const Home = () => {
+  return (
+    <>
+        <h1 className="">Home</h1>
+    </>
+  )
+}
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/App.css', './src/components/**/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
